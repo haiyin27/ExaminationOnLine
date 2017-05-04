9a00bd4e8bbcf1dfeb131cdee92b13deee3bdb41
haiyin <caijianjun@neurotech.cn>
2017-05-04 16:01:23 +0800

    web表单

--- app/app.py
+++ app/app.py
@@ -1,22 +1,43 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from flask_bootstrap import Bootstrap
+from flask import session
+from flask import redirect
+from flask import url_for
 from flask import render_template
 from flask import make_response
-from datetime import datetime
+from flask import flash
 from flask_moment import Moment
 from flask_script import Manager
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField
+from wtforms import SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)  # pylint: disable=invalid-name
+
+app.config['SECRET_KEY'] = 'HfBaNk'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
